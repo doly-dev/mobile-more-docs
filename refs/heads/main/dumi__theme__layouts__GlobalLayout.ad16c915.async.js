978b902d0d31fec0a31ec7aeeb921b8cb7cd4b1f
caijf <jinfeng.cai@qq.com>
2025-12-26 17:20:05 +0800

    Delete refs directory

--- refs/heads/main/dumi__theme__layouts__GlobalLayout.ad16c915.async.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmobile_more=self.webpackChunkmobile_more||[]).push([[1032],{65507:function(d,e,_){_.r(e);var l=_(48305),m=_.n(l),u=_(75271),o=_(81064),n=function(){var t=(0,o.pC)(),s=(0,o.OI)(),a=m()(s,1),r=a[0];return u.useEffect(function(){document.documentElement.setAttribute("data-prefers-color-scheme",r)},[r]),t};e.default=n}}]);
